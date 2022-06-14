85cc8cfe1cb06e0fee3b5d293da10aea0058a0b4
Emerson Millard <emillard.code@gmail.com>
2022-06-14 12:00:00 -0700

    Added Eureka Server annotation to main class.

--- src/main/java/com/project/microservicesdcb3/MicroservicesDcb3Application.java
+++ src/main/java/com/project/microservicesdcb3/MicroservicesDcb3Application.java
@@ -2,12 +2,16 @@ package com.project.microservicesdcb3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroservicesDcb3Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MicroservicesDcb3Application.class, args);
+
     }
 
 }
